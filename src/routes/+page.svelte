69652c443241254c6f8a0f704cfd89efd29d39a3
Matze Schmidbauer <m@neun.dev>
2024-11-15 09:25:22 +0100

    initialize project structure with configuration files and basic setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import * as m from '$lib/paraglide/messages.js'
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
